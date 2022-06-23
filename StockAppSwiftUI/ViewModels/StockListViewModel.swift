bc68d20d9c0910e35f5816c96e6f115a9dee6dc4
Kris Mitina <kristmitina@gmail.com>
2022-06-23 14:24:02 +0200

    Implementing StockListViewModel and StockViewModel

---
+++ StockAppSwiftUI/ViewModels/StockListViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  StockListViewModel.swift
+//  StockAppSwiftUI
+//
+//  Created by DVKSH on 22.06.22.
+//
+
+import Foundation
+
+class StockListViewModel {
+    
+    var searchTerm: String = ""
+    
+    var stocks: [StockViewModel] = [StockViewModel]()
+    
+}
---
+++ StockAppSwiftUI/ViewModels/StockViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  StockViewModel.swift
+//  StockAppSwiftUI
+//
+//  Created by DVKSH on 23.06.22.
+//
+
+import Foundation
+
+struct StockViewModel {
+    
+    let stock: Stock
+    
+    var symbol: String {
+        return self.stock.symbol.uppercased()
+    }
+    
+    var description: String {
+        return self.stock.description
+    }
+    
+    var price: String {
+        return String(format: "%.2f", self.stock.price)
+    }
+    
+    var change: String {
+        return self.stock.change
+    }
+}
